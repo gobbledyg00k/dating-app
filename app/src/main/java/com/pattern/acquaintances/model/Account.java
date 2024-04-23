23278781797e749094d5eb5a6dc023409a29d188
m.pomogaev <m.pomogaev@g.nsu.ru>
2024-04-23 22:08:16 +0700

    New reg\auth functionality and Account management

--- app/src/main/java/com/pattern/acquaintances/model/Account.java
+++ app/src/main/java/com/pattern/acquaintances/model/Account.java
@@ -1,5 +1,41 @@
 package com.pattern.acquaintances.model;
-
 public class Account {
+    private String firstName;
+    private String lastName;
+    private DayOfBirth dayOfBirth;
+    private String location;
+    public  Account(){}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public DayOfBirth getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+    public void setDayOfBirth(DayOfBirth dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
+
