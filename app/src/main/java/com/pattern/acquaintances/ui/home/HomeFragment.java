5b7767427955db71a3fd9a07ab848f30093921d0
Lawantar <nikitin_borya@list.ru>
2024-06-05 11:37:18 +0700

    Some workable thing...

--- app/src/main/java/com/pattern/acquaintances/ui/home/HomeFragment.java
+++ app/src/main/java/com/pattern/acquaintances/ui/home/HomeFragment.java
@@ -1,12 +1,15 @@
 package com.pattern.acquaintances.ui.home;
 
 import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.os.Handler;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,7 +17,6 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.pattern.acquaintances.MainActivity;
 import com.pattern.acquaintances.databinding.FragmentHomeBinding;
 import com.pattern.acquaintances.model.Account;
@@ -23,22 +25,25 @@ import com.pattern.acquaintances.model.DBManager;
 import com.pattern.acquaintances.model.StorageManager;
 
 import java.util.function.Function;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
     private String email;
     private String pass;
-
+    Handler mainHandler;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         StorageManager storage = new StorageManager();
+        mainHandler = new Handler();
         storage.setGetProfileOnCompete(new OnCompleteListener<Uri>() {
             @Override
             public void onComplete(@NonNull Task<Uri> task) {
                 if (task.isSuccessful()){
-                    binding.imageView2.setImageURI(task.getResult());
+                    new ImageDownloader(task.getResult().toString()).start();
                 }
             }
         });
@@ -67,5 +72,33 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    class ImageDownloader extends Thread{
+        String URL;
+        Bitmap bitmap;
+        ImageDownloader(String url){
+            URL = url;
+        }
+
+        @Override
+        public void run(){
+            super.run();
+            InputStream stream;
+            try {
+                stream = new java.net.URL(URL).openStream();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            bitmap = BitmapFactory.decodeStream(stream);
+
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    binding.imageView2.setImageBitmap(bitmap);
+                    Log.i("Storage", "Image set");
+                }
+            });
+        }
     }
 }
